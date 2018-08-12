d03aea3527f805e748d1da780b97b83c20f7dddb
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-08-12 18:50:59 +0530

    Add files via upload

---
+++ mat.java
@@ -0,0 +1,15 @@
+package multithread;
+
+public class mat {
+
+
+	    public static void main(String[] args) {
+
+	        int num = 8;
+	        for(int i = 1; i <= 10; ++i)
+	        {
+	            System.out.print("%d * %d = %d \n", num, i, num * i);
+	        }
+	    }
+	}
+
